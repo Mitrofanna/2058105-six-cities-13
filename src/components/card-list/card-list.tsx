422b211a1565a298b61b5c99f20f8c10bd50c1ef
Mitrofanna <mitrofanovaa70@gmail.com>
2023-10-25 22:47:32 +0300

    выносит сортировку в отдельный компонент

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -2,6 +2,7 @@ import Card from "../card/card";
 import { CardProps } from "../../types/card";
 import Map from "../map/map";
 import { JSX } from "react/jsx-runtime";
+import Sorting from "../sorting/sorting";
 
 type CardListProps = {
     offersSorted: Record<string, CardProps[]>;
@@ -11,46 +12,31 @@ type CardListProps = {
 	handleMouseLeave: () => void;
 }
 
-function CardList({handleMouseEnter, handleMouseLeave, selectedCity, offersSorted, currentCity}: CardListProps): JSX.Element {
-    
+function CardList({handleMouseEnter, handleMouseLeave, selectedCity, offersSorted, currentCity}: CardListProps): JSX.Element {  
     return (
         <div className="cities__places-container container">
-						<section className="cities__places places">
-							<h2 className="visually-hidden">Places</h2>
-							<b className="places__found">{offersSorted[selectedCity].length} places to stay in {selectedCity}</b>
-							<form className="places__sorting" action="#" method="get">
-								<span className="places__sorting-caption">Sort by</span>{' '}
-								<span className="places__sorting-type" tabIndex={0}>
-									Popular
-									<svg className="places__sorting-arrow" width="7" height="4">
-										<use xlinkHref="#icon-arrow-select"></use>
-									</svg>
-								</span>
-								<ul className="places__options places__options--custom places__options--opened">
-									<li className="places__option places__option--active" tabIndex={0}>Popular</li>
-									<li className="places__option" tabIndex={0}>Price: low to high</li>
-									<li className="places__option" tabIndex={0}>Price: high to low</li>
-									<li className="places__option" tabIndex={0}>Top rated first</li>
-								</ul>
-							</form>
-							<div className="cities__places-list places__list tabs__content">
-								{offersSorted[selectedCity].map((card) => <Card
-								card={card}
-								key={card.id}
-								onMouseEnter={() => handleMouseEnter(card.id)}
-                                onMouseLeave={handleMouseLeave}
-								/>)}
-							</div>
-						</section>
-						<div className="cities__right-section">
-						<Map
-							city={offersSorted[selectedCity][0]}
-							points={offersSorted[selectedCity]}
-							selectedPoint={currentCity}
-							isMainPage
-						/>
-						</div>
-					</div>
+			<section className="cities__places places">
+				<h2 className="visually-hidden">Places</h2>
+				<b className="places__found">{offersSorted[selectedCity].length} places to stay in {selectedCity}</b>
+				<Sorting />
+				<div className="cities__places-list places__list tabs__content">
+					{offersSorted[selectedCity].map((card) => <Card
+					card={card}
+					key={card.id}
+					onMouseEnter={() => handleMouseEnter(card.id)}
+					onMouseLeave={handleMouseLeave}
+					/>)}
+				</div>
+			</section>
+			<div className="cities__right-section">
+			<Map
+				city={offersSorted[selectedCity][0]}
+				points={offersSorted[selectedCity]}
+				selectedPoint={currentCity}
+				isMainPage
+			/>
+			</div>
+		</div>
     );
 }
 
