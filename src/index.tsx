fbf912f7baa89245acb8a9d29b435ac69aadd3c1
Mitrofanna <mitrofanovaa70@gmail.com>
2023-10-25 19:07:52 +0300

    подключает redux к проекту

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import cards from './mocks/offer-mocks';
-import {reviews} from './mocks/review-mocks';
-
+import { reviews } from './mocks/review-mocks';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -11,6 +12,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
 	<React.StrictMode>
+		<Provider store={store}>
 		<App cards={cards} reviews={reviews}/>
+		</Provider>
 	</React.StrictMode>
 );
--- src/types/state.ts
+++ src/types/state.ts
@@ -1,4 +1,9 @@
 import { CardProps } from "./card";
+import {store} from '../store/index.ts';
+
+export type State = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
 
 export type InitialState = {
 	selectedCity: string | undefined;
